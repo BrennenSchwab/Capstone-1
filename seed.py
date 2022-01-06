7615f76abed917b021eee0848bf8b1326d580192
halimt92 <48407395+halimt92@users.noreply.github.com>
2022-01-06 23:02:19 +0100

    fixes halim 6 jan (#1)
    Co-authored-by: halim.tannous <halim.tannous@gmail.com>

--- seed.py
+++ seed.py
@@ -1,23 +1,29 @@
 from nba_api.stats.library import parameters, data
 from nba_api.stats.static import teams, players
-from models import Player, Team
+from models import Player
 from app import db
 
 db.drop_all()
 db.create_all()
 
+
 def seed_basic_player_info():
-    """ Get general info for a player and store it"""
+    """Get general info for a player and store it"""
     players_list = []
 
     players_list = players.get_active_players()
-
-    player_info = Player(
-        id = players_list['id'],
-        first_name = players_list['first_name'],
-        last_name = players_list['last_name'],
-        full_name = players_list['full_name'],
-    )
-
-    db.session.add(player_info)
+    all_players = []
+    for player in players_list:
+        all_players.append(
+            Player(
+                id=player["id"],
+                first_name=player["first_name"],
+                last_name=player["last_name"],
+                full_name=player["full_name"],
+            )
+        )
+    db.session.bulk_save_objects(all_players)
     db.session.commit()
+
+
+seed_basic_player_info()
