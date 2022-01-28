1fa4e87219cd61b56177270955b77a331e661d8c
BrennenSchwab <brennen.schwab@gmail.com>
2022-01-27 17:26:51 -0800

    jan27

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgres
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
-app.config["SECRET_KEY"] = "p-word-here-shhhhh"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "p-word-here-shhhhh")
 
 connect_db(app)
 
