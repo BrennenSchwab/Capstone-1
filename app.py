f7f328f3f0b9fb52975ecf66d67c23e7ab431109
BrennenSchwab <brennen.schwab@gmail.com>
2022-04-14 15:04:22 -0700

    m

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgresql:///nba_stats")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
